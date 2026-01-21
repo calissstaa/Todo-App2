a0916e844022575e78aea9e311fc35acd1f6048b
calissstaa <calissstaa@gmail.com>
2026-01-21 19:14:17 +0700

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-transparent text-white py-4">
+      <div className="container mx-auto px-4 text-center text-sm">
+        To do list by Calista, Astrid, Delfin
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
